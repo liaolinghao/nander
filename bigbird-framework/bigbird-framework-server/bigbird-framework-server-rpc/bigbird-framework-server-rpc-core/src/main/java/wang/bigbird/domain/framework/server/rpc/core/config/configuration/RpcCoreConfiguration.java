dd7b328a656a424a66611fbf7703d1cd38bf9cd2
大鸟 <26089183@qq.com>
2026-07-23 08:53:55 +0800

    增加服务注册类

--- bigbird-framework/bigbird-framework-server/bigbird-framework-server-rpc/bigbird-framework-server-rpc-core/src/main/java/wang/bigbird/domain/framework/server/rpc/core/config/configuration/RpcCoreConfiguration.java
+++ bigbird-framework/bigbird-framework-server/bigbird-framework-server-rpc/bigbird-framework-server-rpc-core/src/main/java/wang/bigbird/domain/framework/server/rpc/core/config/configuration/RpcCoreConfiguration.java
@@ -18,6 +18,7 @@ import com.alibaba.nacos.api.naming.NamingService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
